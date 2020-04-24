53ffb776ecfd030b35096ab67b31699798230a71
Yang Zhang <yangzhang@nvidia.com>
2020-04-24 16:44:53 -0700

    working: sys cat slot val replacement, user non-cat slot value replacement, non-slot replamcent
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- examples/nlp/dialogue_state_tracking/data/augment_sgd.py
+++ examples/nlp/dialogue_state_tracking/data/augment_sgd.py
@@ -21,56 +21,48 @@ for schema in orig_schema:
         slot_name = slot['name']
         is_categorical[(service_name, slot_name)] = slot['is_categorical']
 ontology = {}
-#4.
-def replace_frame_values(turn, service, key, old_value, new_value):
-    assert(isinstance(old_value, str))
-    assert(isinstance(new_value, str))
-    """
-    sentence (str):
-    st_ch_idx (int): char start index in sentence
-    exclusive_end_ch_idx (int): char end index in sentence
-    status_updates (List[dict]) list of updated frames 
-    """
-    offset = len(new_value) - len(old_value)
-    # update spans
-    # check if st_ch_idx <  start idx of any status update non-categorial, if so start idx+= offset len(new_value) - len(word_to_replace)
-    # check if st_ch_idx < end idx of  any status update non-categorial, if so end idx += offset len(new_value) - len(word_to_replace)
-    for frame in turn["frames"]:
-        if frame["service"] == service:
-            st_ch_idx, exclusive_end_ch_idx = frame['slot_to_span'].get(key, {old_value: (-1, -1)}).get(old_value, (-1, -1))
-            if st_ch_idx != -1:
-                for slot in frame.get('slots', []):
-                    if st_ch_idx < slot['start']:
-                        slot['start'] += offset
-                    if st_ch_idx < slot['exclusive_end']:
-                        slot['exclusive_end'] += offset
-        
-            is_sys_utt = "actions" in frame
-            if is_sys_utt:
-                for action in frame["actions"]:
-                    if key == action["slot"] and old_value in action["values"]:
-                        action["values"].remove(old_value)
-                        action["values"].append(new_value)
-            else:
-                for k, v in frame["state"]["slot_values"].items():
-                    if k == key and v[0] == old_value:
-                        v[0] = new_value
 
-            # update slot_to_span if old_val in utterance
-            if st_ch_idx != -1:
-                for slot_name, val_spans in frame["slot_to_span"].items():
-                    for slot_val, spans in val_spans.items():
-                        if spans[0] == -1:
-                            continue
-                        if slot_name == key and slot_val == old_value:
-                            frame["slot_to_span"][slot_name].pop(slot_val)
-                            frame["slot_to_span"][slot_name][new_value] = spans
-                        if st_ch_idx < spans[0]:
-                            spans[0] += offset
-                        if st_ch_idx < spans[1]:
-                            spans[1] += offset 
-                # update utterance
-                turn["utterance"] = turn["utterance"][:st_ch_idx] + new_value +  turn["utterance"][exclusive_end_ch_idx:]
+def update_spans(dialogue, turn_id, frame_id, start_idx, end_idx, old_value, new_value):
+    """
+    update slot spans and slot_to_span
+    """
+    frame = dialogue["turns"][turn_id]["frames"][frame_id]
+    offset = len(new_value) - len(old_value)
+
+    for slot in frame['slots']:
+        if start_idx < slot['start']:
+            slot['start'] += offset
+        if start_idx < slot['exclusive_end']:
+            slot['exclusive_end'] += offset
+    
+    for k, vs in frame['slot_to_span'].items():
+        for v, spans in vs.items():
+            if start_idx < spans[0]:
+                spans[0] += offset
+            if start_idx < spans[1]:
+                spans[1] += offset
+        
+
+def update_values(dialogue, turn_id, frame_id, key, old_value, new_value):
+    """
+    only update values: actions, state, slot_to_span
+    """
+    frame = dialogue["turns"][turn_id]["frames"][frame_id]
+    if "actions" in frame:
+        for action in frame["actions"]:
+            if key == action["slot"] and old_value in action["values"]:
+                action["values"].remove(old_value)
+                action["values"].append(new_value)
+    if "state" in frame:
+        for k, v in frame["state"]["slot_values"].items():
+            if k == key and v[0] == old_value:
+                v[0] = new_value
+
+    for k, vs in frame["slot_to_span"].items():
+        for v, spans in vs.items():
+            if k == key and v == old_value:
+                frame["slot_to_span"][k].pop(v)
+                frame["slot_to_span"][k][new_value] = spans
 
 
 def get_affected_future_frames(dialogue, from_turn_id, slot_name, slot_value, service):
@@ -86,11 +78,11 @@ def get_affected_future_frames(dialogue, from_turn_id, slot_name, slot_value, se
                 if frame["service"] == service:
                     for action in frame["actions"]:
                         if action["slot"] == slot_name and slot_value in action["values"]:
-                            res.append((turn_id, frame_id, service, slot_name))
+                            res.append((turn_id, frame_id, slot_name))
                             continue
             else:
                 if frame["service"] == service and frame["state"]["slot_values"].get(slot_name, [None])[0] == slot_value:
-                    res.append((turn_id, frame_id, service, slot_name))
+                    res.append((turn_id, frame_id, slot_name))
                     continue
     return res
 
@@ -177,56 +169,73 @@ def get_sentence_components(turn):
         idx += 1
     return res
             
-def find_word_in_turn(word, turn):
-    assert(isinstance(word, str))
-    frames = turn["frames"]
+def find_word_in_turn(dialogue, turn_id, value, start_idx, end_idx):
+    """
+    find value only in new state_update
+    dialogue
+    turn_id
+    value
+    start_idx
+    end_idx
+
+    return  List[(turn_id, frame_id, key)]
+    """
+    assert(isinstance(value, str))
+    frames = dialogue["turns"][turn_id]["frames"]
+    res = []
     for frame_id, frame in enumerate(frames):
-        
         if "state" in frame: 
             for k, v in frame["state"]["slot_values"].items():
-                if k in frame["state_update"] and word == v[0]:
-                    return frame_id, frame["service"], k, word
+                if k in frame["state_update"] and value == v[0]:
+                    res.append((turn_id, frame_id, k))
         if "actions" in frame:
+            # system doesnt need state_update
             for action in frame["actions"]:
                 k = action["slot"]
                 for v in action["values"]:
-                    if v == word:
-                        return frame_id, frame["service"], k, word
-    return None
+                    if v == value:
+                        res.append((turn_id, frame_id, k))
+    return res
 
 # 3. 
-def replace(dialogue, turn_id, start_idx, end_idx, new_value, ontology):
+def replace(dialogue, turn_id, start_idx, end_idx, new_value):
     assert(isinstance(turn_id, int))
     assert(isinstance(start_idx, int))
     assert(isinstance(end_idx, int))
     """
-    frame in turn has auxiliary new slot key
-    ontology: (service, slot) = [list of all seen values/ possible values]
-    TODO: add for each frame mapping from ALL slot value -> span in utterance
-
+    does old_value segment start_idx : end_idx correspond to a new slot_value v of key k some frame f?
+    if no, do nothing
+    if yes 
+        determine new value
+        depends whether slot is categorical or not
+        replace_utterance_and_span(turn, start_idx, end_idx, new_value)
+        replace_frame_state_values(old_value, new_value, f, k)
+        for each affected frame f' that is linked in frame f
+            replace_frame_state_values(old_value, new_value, f', k)
     """
-    # does old_value segment start_idx : end_idx correspond to a new slot_value v of key k some frame f?
-    # if no, do nothing
-    # if yes 
-        # determine new value
-        # depends whether slot is categorical or not
-        # replace_utterance_and_span(turn, start_idx, end_idx, new_value)
-        # replace_frame_state_values(old_value, new_value, f, k)
-        # for each affected frame f' that is linked in frame f
-            # replace_frame_state_values(old_value, new_value, f', k)
     turn = dialogue["turns"][turn_id]
     sentence = turn["utterance"]
     old_value = sentence[start_idx: end_idx]
-    found = find_word_in_turn(old_value, turn)
-    if found:
-        frame_id, service, key, _ = found
-        frame = turn["frames"][frame_id]
-
-        affected_frames = [(turn_id, frame_id, service, key)]
-        affected_frames += frame["state_update"][key]
-        for affected in affected_frames:
-            affected_turn_id, affected_frame_id, affected_service, affected_slot = affected
-            replace_frame_values(dialogue["turns"][affected_turn_id], affected_service, affected_slot,  old_value, new_value)
+    affected_values = find_word_in_turn(dialogue=dialogue, turn_id=turn_id, value=old_value, start_idx=start_idx, end_idx=end_idx)
+    affected_spans = [(turn_id, start_idx, end_idx)]
+    for _, frame_id, key in affected_values.copy():
+        frame = dialogue["turns"][turn_id]["frames"][frame_id]
+        new_affected_values = frame["state_update"][key]
+        affected_values += new_affected_values
+        for a_turn_id, a_frame_id, a_key in new_affected_values:
+            assert(key==a_key)
+            spans=dialogue["turns"][a_turn_id]["frames"][a_frame_id]["slot_to_span"].get(a_key, {}).get(old_value, None)
+            if spans:
+                affected_spans += [(a_turn_id, spans[0], spans[1])]
+    
+    for a_turn_id, a_frame_id, a_key in affected_values:
+        assert(a_key==key)
+        update_values(dialogue, a_turn_id, a_frame_id, a_key, old_value, new_value)
+    for a_turn_id, start_idx, end_idx in affected_spans:
+        for a_frame_id in range(len(dialogue["turns"][a_turn_id]["frames"])):
+            update_spans(dialogue, a_turn_id, a_frame_id, start_idx, end_idx, old_value, new_value)
+        # update utterance
+        dialogue["turns"][a_turn_id]["utterance"] = dialogue["turns"][a_turn_id]["utterance"][:start_idx] + new_value + dialogue["turns"][a_turn_id]["utterance"][end_idx:]
             
 def digit2str(x):
     x = x.split()
@@ -234,8 +243,6 @@ def digit2str(x):
         if x[i].is_digit():
             x[i] = num2words(x[i])
     return " ".join(x)
-
-
 
 def validate(dialogue):
     # check slot spans match utterance and state value
@@ -250,35 +257,41 @@ def validate(dialogue):
                     for action in frame["actions"]:
                         if action["slot"] == key:
                             found_key = True
-                            assert(turn["utterance"][st_idx: end_idx] in action["values"])
+                            try:
+                                assert(turn["utterance"][st_idx: end_idx] in action["values"])
+                            except:
+                                raise ValueError
                     assert(found_key)
                 else:
                     assert(key in frame["state"]["slot_values"])
-                    assert(turn["utterance"][st_idx: end_idx] == frame["state"]["slot_values"][key][0])
+                    try:
+                        assert(turn["utterance"][st_idx: end_idx] == frame["state"]["slot_values"][key][0])
+                    except:
+                        raise ValueError
 
 
 
 
-def test_helper(dialogues, dialogue_id, turn_id, span_id, new_word):
+def test_helper(dialogues, dialogue_id, turn_id, span_id, new_value):
     dialogue = copy.deepcopy(dialogues[dialogue_id])
     augment_dialog_by_auxiliary_entries(dialogue)
     spans = get_sentence_components(dialogue["turns"][turn_id])
-    replace(dialogue, turn_id, start_idx=spans[span_id][0], new_value=new_word,  end_idx=spans[span_id][1], ontology=ontology)
+    replace(dialogue, turn_id=turn_id, start_idx=spans[span_id][0], end_idx=spans[span_id][1], new_value=new_value)
 
     for turn in dialogue["turns"]:
         for frame in turn["frames"]:
             if "state_update" in frame:
                 frame.pop("state_update")
+    pprint(dialogue)
     validate(dialogue)
     d_str_new = json.dumps(dialogue, sort_keys=True, indent=2)
     d_str_old = json.dumps(dialogues[dialogue_id], sort_keys=True, indent=2)
-    pprint(dialogue)
     print(d_str_new == d_str_old)
 
 
-# test_helper(orig_dialog, dialogue_id=1, turn_id=5, span_id=3, new_word="EXPENSIVEEE") # system cat
-# test_helper(orig_dialog, dialogue_id=0, turn_id=2, span_id=-1, new_word="MIAMIIII") # user non-cat
-test_helper(orig_dialog, dialogue_id=0, turn_id=12, span_id=14, new_word="moderate")
+# test_helper(orig_dialog, dialogue_id=1, turn_id=5, span_id=3, new_value="EXPENSIVEEE") # system cat, moderate
+# test_helper(orig_dialog, dialogue_id=0, turn_id=2, span_id=-1, new_value="MIAMIIIIIIIIIIIII") # user non-cat, San Jose
+# test_helper(orig_dialog, dialogue_id=0, turn_id=12, span_id=14, new_value="MODERATE") # replace value that does not match slot
 
 
 
