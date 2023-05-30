cfc3e779b8342f420477ba5acb387af177b231d2
Yang Zhang <yangzhang@nvidia.com>
2023-05-30 09:52:47 -0700

    added memory efficient output embedding tp hotfix
    Signed-off-by: Yang Zhang <yangzhang@nvidia.com>

--- examples/nlp/language_modeling/tuning/megatron_gpt_peft_eval.py
+++ examples/nlp/language_modeling/tuning/megatron_gpt_peft_eval.py
@@ -166,17 +166,17 @@ def main(cfg) -> None:
                 for batch in response:
                     batch_sentences = [s for s in batch['sentences']]
                     batch_tokens = [s for s in batch['tokens']]
-                    batch_logprob = [s.tolist() for s in batch['logprob']]
-                    for s, t, l in zip(batch_sentences, batch_tokens, batch_logprob):
-                        if cfg.inference.get("verbose", False):
-                            d = {
-                                'sentence': s,
-                                'tokens_with_logprobs': ', '.join([f"{_t} {_l:.4f}" for _t, _l in zip(t, l)]),
-                            }
-                            f.write(json.dumps(d, sort_keys=True, indent=2) + '\n')
-                        else:
-                            d = {'sentence': s}
-                            f.write(json.dumps(d) + '\n')
+                    # batch_logprob = [s.tolist() for s in batch['logprob']]
+                    for s, t in zip(batch_sentences, batch_tokens):
+                        # if cfg.inference.get("verbose", False):
+                        #     d = {
+                        #         'sentence': s,
+                        #         # 'tokens_with_logprobs': ', '.join([f"{_t} {_l:.4f}" for _t, _l in zip(t, l)]),
+                        #     }
+                        #     f.write(json.dumps(d, sort_keys=True, indent=2) + '\n')
+                        # else:
+                        d = {'sentence': s}
+                        f.write(json.dumps(d) + '\n')
             print("predictions saved to {}".format(cfg.inference.outfile_path))
         else:
             print(response)
